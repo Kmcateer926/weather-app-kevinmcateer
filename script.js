3deb4877af05812507b59d2fb4ff59518697c3a1
Kevin McAteer <kmcateer926@gmail.com>
2020-10-11 17:33:00 -0400

    attempting to link api call

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+console.log("Hello World!");
+$(document).ready(function () {
+  console.log("ready");
+  $("#searchInput").on("click", function (event) {
+    event.preventDefault();
+
+    var queryURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=Athens&appid=c25eda49392ed989a410cf267c3525ce";
+    $.ajax({
+      Url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+  });
+});
