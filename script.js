a021d6b23b4a430e67e1ac932263e88a134f662c
Kevin McAteer <kmcateer926@gmail.com>
2020-10-11 17:55:30 -0400

    attempting to pull API based on search input

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("Hello World!");
 $(document).ready(function () {
   console.log("ready");
-  $("#searchInput").on("click", function (event) {
+  $("#search").on("click", function (event) {
     event.preventDefault();
-
+var city = $("#searchInput").val()
     var queryURL =
-    "http://api.openweathermap.org/data/2.5/forecast?q=Athens&appid=c25eda49392ed989a410cf267c3525ce";
+    "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c25eda49392ed989a410cf267c3525ce";
     $.ajax({
-      Url: queryURL,
+      url: queryURL,
       method: "GET",
     }).then(function (response) {
       console.log(response);
