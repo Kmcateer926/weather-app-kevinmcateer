14ec3a87388597a894228a7f8d322756b0a57494
Kevin McAteer <kmcateer926@gmail.com>
2020-10-11 21:37:35 -0400

    attempting to populate API information

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ $(document).ready(function () {
   console.log("ready");
   $("#search").on("click", function (event) {
     event.preventDefault();
-var city = $("#searchInput").val()
-console.log(city)
+    var city = $("#searchInput").val();
+    console.log(city);
     var queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=c25eda49392ed989a410cf267c3525ce";
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      city +
+      "&appid=c25eda49392ed989a410cf267c3525ce";
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
       console.log(response);
+      console.log(response.city);
+      console.log(
+        "The current forecast calls for " + response.weather[0].description
+      );
+      console.log("The current temperature is " + response.main.temp + "F");
+      console.log("Low:" + response.main.temp_min + "F");
+      console.log("High:" + response.main.temp_max + "F");
+      console.log("Humidity currently at " + response.main.humidity + "%");
+      console.log(
+        "Wind is moving at a speed of:" + response.wind.speed + "mph"
+      );
     });
   });
 });
+// });
